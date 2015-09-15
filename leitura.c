f734c92875925b7197975b8fda9f2829a1aa643d
Nicholas Brant <nicbrant@gmail.com>
2015-09-16 04:52:29 +0800

    Enviando .c s

---
+++ leitura.c
@@ -0,0 +1,15 @@
+
+/* Armazena os dados do arquivo 'palestrantes.txt'
+* numa lista do tipo 'ArmazenaDispo'
+*/
+
+ArmazenaDispo* leArquivo1(FILE *arquivo1) {
+
+}
+
+/* Armazena os dados do arquivo 'palestras.txt'
+* numa lista do tipo 'ArmazenaPalestra'
+*/
+ArmazenaPalestra* leArquivo2(FILE *arquivo2) {
+
+}
