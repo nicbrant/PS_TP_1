a4b2d0d5c231cf0292001061158c64e134c93d8d
nicholas brant <nicbrant@gmail.com>
2015-12-05 23:43:06 -0200

    changes

--- leitura.h
+++ leitura.h
@@ -1,20 +1,3 @@
-/*Estrutura para guardar dados do arquivo 'palestrantes.txt'*/
-struct armazenaDispo /*Vamos nos referir a lista dela como "arquivo1"*/
-{
-	char nomePalestrante[100];
-	char dispo[1000];
-	struct armazena *prox;
-};
-typedef struct armazenaDispo ArmazenaDispo;
-
-/*Estrutura para guardar dados do aquivo 'palestras.txt'*/
-struct armazenaPalestra /*Vamos nos referir a lista dela como "arquivo2"*/
-{
-	char nomePessoa[50], nomePalestra[50], tema[50], local[50];
-	int duracao;
-};
-typedef struct armazenaPalestra ArmazenaPalestra;
-
 /* Armazena os dados do arquivo 'palestrantes.txt'
  * numa lista do tipo 'ArmazenaDispo'
  */
